74ce5e8f6e297497668877972b545d330e53a4f3
Łukasz Susłowicz <susel.invest@gmail.com>
2023-01-24 18:59:18 +0100

    adding new tasks and showing if task is done

--- js/script.js
+++ js/script.js
@@ -16,7 +16,11 @@
 
         for (const task of tasks) {
             htmlString += `
-                <li>
+                <li
+                  class="list__item${task.done ? " list__item--done" : ""}"
+                >
+                  <button class = "js_done">zrobione?</button>
+                  <button class = "js-remove">usuń</button>
                     ${task.content}
                 </li>
             `;
@@ -24,9 +28,40 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
+    const addNewTask = (newTaskContent) => {
+      tasks.push({
+        content: newTaskContent,
+      });
+      render();
+    };
+
+
+
+    // function eraseInput() {
+    //     const inputToErase = doument.querySelector(".js-newTask");
+    //     inputToErase.value = "";
+
+    // };
+    const onFormSubmit = (event) => {
+      event.preventDefault();
+
+      const newTaskContent = document.querySelector(".js-newTask").value.trim();
+      console.log(newTaskContent);
+
+      if (newTaskContent === "") {
+        return;
+      }
+      addNewTask(newTaskContent);
+      // eraseInput();
+
+    };
+
     const init = () => {
         render();
 
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
